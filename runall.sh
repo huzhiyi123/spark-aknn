b5779f0aeecd9372995329239806206881bf5ff2
yaoheng <sjtu_xyh@sjtu.edu.cn>
2022-02-05 21:13:02 +0800

    clean fold

--- runall.sh
+++ runall.sh
@@ -3,9 +3,7 @@
 #bash 5.sh 20.log
 #nohup bash /runall.sh &>> cur4.log&
 #bash 2.sh 1.log
-log=2.log
 aknnfold=/aknn
-logfold=/aknn/alllog/1/
 cd $aknnfold
 bash 5.sh $1
 #cat test/params.py >> $logfold$log
--- runtest.sh
+++
@@ -1,9 +0,0 @@
-#!bash
-#bash 2.sh 19.log
-#bash 5.sh 20.log
-#nohup bash /runall.sh &>> cur4.log&
-#bash 2.sh 1.log
-aknnfold=/aknn
-cd $aknnfold
-bash 2.sh $1
-
